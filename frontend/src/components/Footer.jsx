f15c0b041eb954be43dc8faab262f7be2f409acc
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-02 12:55:20 -0300

    feat : agregando navegacion preliminar (router) - sin estilos

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
+import { Link as LinkRouter } from 'react-router-dom';
+import '../styles/footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faUser, faPercent, faClipboardList, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons'
 
+
+
+
+const pages = [
+  {
+    name: 'Inicio',
+    path: '/inicio',
+    icon: faHouse
+
+  },
+  {
+    name: 'Perfil',
+    path: '/perfil',
+    icon: faUser
+  },
+  {
+    name: 'Beneficios',
+    path: '/beneficios',
+    icon: faPercent
+  },
+  {
+    name: 'Pedidos',
+    path: '/pedidos',
+    icon: faClipboardList
+  },
+  {
+    name: 'Pagos',
+    path: '/pagos',
+    icon: faFileInvoiceDollar
+  }
+];
 
 function Footer() {
   return (
     <>
-    <p>Footer</p>
+      <footer className='footer'>
+        <nav style={{ width: '98%', marginLeft:'1%', marginRight:'1%' }}>
+          <ul style={{ width: '100%', display: 'flex', flexDirection: 'row', columnGap: '20px', justifyContent: 'center' }}>
+            {pages.map((page) => (
+              <li key={page.path} style={{ listStyleType: 'none' }}>
+                <LinkRouter to={page.path}>
+
+                  <FontAwesomeIcon icon={page.icon}
+                    style={{ fontSize: '20px' }} />
+                  <p style={{ fontSize: '14px' }}>{page.name}</p>
+                </LinkRouter>
+              </li>))}
+          </ul>
+
+        </nav>
+
+
+
+
+      </footer>
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
 import { Link as LinkRouter } from 'react-router-dom';
+import '../styles/navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faBell, faCartShopping} from '@fortawesome/free-solid-svg-icons'
+
 const pages = [
   {
-    name: 'Inicio',
-    path: '/inicio'
+    name: 'Notificaciones',
+    path: '/notificaciones',
+    icon: faBell
   },
   {
-    name: 'Perfil',
-    path: '/perfil'
+    name: 'Carrito',
+    path: '/carrito',
+    icon:faCartShopping
   },
-  {
-    name: 'Beneficios',
-    path: '/beneficios'
-  },
-  {
-    name: 'Pedidos',
-    path: '/pedidos'
-  },
-  {
-    name: 'Pagos',
-    path: '/pagos'
-  }
+  
 ];
 function Navbar() {
   return (
     <>
-      <div>
-        <nav>
-          <ul>
+      <header>
+        <nav >
+          <ul style={{width:'90%', display:'flex', flexDirection:'row', justifyContent:'flex-end', columnGap:'20px', marginRight:'50px'}}>
             {pages.map((page) => (
-              <li key={page.path}>
-                <LinkRouter to={page.path}>{page.name}</LinkRouter>
+              <li key={page.path} style={{listStyleType:'none' }}>
+                <LinkRouter to={page.path}><FontAwesomeIcon icon={page.icon} /></LinkRouter>
               </li>))}
           </ul>
 
@@ -39,7 +34,7 @@ function Navbar() {
 
 
 
-      </div>
+      </header>
     </>
   )
 }
