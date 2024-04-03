c7aa3651941737e016cb5b889a6c73197c9f074d
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-03 12:59:54 -0300

    creando proyecto y definiendo la arquitectura del proyecto

---
+++ backend/src/main/java/com/nocountry/foodlyfinds/FoodlyFindsApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.foodlyfinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlyFindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlyFindsApplication.class, args);
+	}
+
+}
