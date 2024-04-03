7cc20eaaf65c2b63d54893801c6343d288aed6f7
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-03 02:27:40 -0600

    feat: add HamburgerBtn

---
+++ frontend/src/components/Buttons/HamburgerBtn/HamburgerBtn.jsx
@@ -0,0 +1,13 @@
+import './HamburgerBtn.css'
+
+export default function HamburgerBtn() {
+  return (
+    <button className="hamburger-btn" >
+      <svg viewBox='0 0 100 100'>
+        <rect />
+        <rect />
+        <rect />
+      </svg>
+    </button>
+  )
+}
