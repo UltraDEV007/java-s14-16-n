60a910e6ff2bf9a8c9091890abc9e8bce68edc7e
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-02 20:18:41 -0600

    feat: add Normalize in App.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
