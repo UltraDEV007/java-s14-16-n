9f8728e6a3839421d100ab9d092db425e5f6d59c
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-04 20:29:02 -0300

    refactor: products ingredients to string

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -1,102 +1,101 @@
 export const products = [
-    {
+  {
       "productId": "1",
       "storeId": "2",
       "name": "Hamburguesa clásica",
       "categoryId": "1",
       "productImageUrl": "https://drive.google.com/file/d/1yc9zH3HLSvKaW47t8rCh1bTwB4Ft8jDh/view?usp=drivesdk",
-      "ingredients": ["Carne de res", "Queso cheddar", "Lechuga", "Tomate", "Pan de hamburguesa"],
+      "ingredients": "• Carne de res • Queso cheddar • Lechuga • Tomate • Pan de hamburguesa",
       "clarification": "",
       "price": 5000,
       "waitingTime": "10 min"
-    },
-    {
+  },
+  {
       "productId": "2",
       "storeId": "2",
       "name": "Pizza Margarita",
       "categoryId": "2",
       "productImageUrl": "https://drive.google.com/file/d/19CdD1677Kw5V58-6BHLwhAlB4fNzt-lG/view?usp=drivesdk",
-      "ingredients": ["Mozzarella", "Tomate", "Albahaca", "Masa de pizza"],
+      "ingredients": "• Mozzarella • Tomate • Albahaca • Masa de pizza",
       "clarification": "",
       "price": 7000,
       "waitingTime": "15 min"
-    },
-    {
+  },
+  {
       "productId": "3",
       "storeId": "2",
       "name": "Ensalada de atún",
       "categoryId": "3",
       "productImageUrl": "https://drive.google.com/file/d/1_obokewu4NL5_u1f62sVR936qB2BvJOR/view?usp=drivesdk",
-      "ingredients": ["Lechuga", "Atún", "Huevo duro", "Tomate", "Aceitunas", "Cebolla", "Vinagreta"],
+      "ingredients": "• Lechuga • Atún • Huevo duro • Tomate • Aceitunas • Cebolla • Vinagreta",
       "clarification": "",
       "price": 2000,
       "waitingTime": "10 min"
-    },
-    {
+  },
+  {
       "productId": "4",
       "storeId": "1",
       "name": "Pizza Hawaiana",
       "categoryId": "2",
       "productImageUrl": "https://mandolina.co/wp-content/uploads/2023/06/pizza-hawaian-2.jpg",
-      "ingredients": ["Jamón", "Piña", "Mozzarella", "Masa de pizza"],
+      "ingredients": "• Jamón • Piña • Mozzarella • Masa de pizza",
       "clarification": "",
       "price": 8000,
       "waitingTime": "15 min"
-    },
-    {
+  },
+  {
       "productId": "5",
       "storeId": "1",
       "name": "Pizza Pepperoni",
       "categoryId": "2",
       "productImageUrl": "https://i.pinimg.com/736x/2d/41/f7/2d41f7762a4e8b20473caada000641ff.jpg",
-      "ingredients": ["Pepperoni", "Mozzarella", "Masa de pizza", "Salsa de tomate"],
+      "ingredients": "• Pepperoni • Mozzarella • Masa de pizza • Salsa de tomate",
       "clarification": "",
       "price": 7500,
       "waitingTime": "15 min"
-    },
-    {
+  },
+  {
       "productId": "6",
       "storeId": "1",
       "name": "Pizza Vegetariana",
       "categoryId": "2",
       "productImageUrl": "https://cdn.shopify.com/s/files/1/0191/9978/files/Pizza-Veggie-Supreme-blog.jpg?v=1652775259",
-      "ingredients": ["Pimientos", "Cebolla", "Champiñones", "Tomate", "Mozzarella", "Masa de pizza"],
+      "ingredients": "• Pimientos • Cebolla • Champiñones • Tomate • Mozzarella • Masa de pizza",
       "clarification": "",
       "price": 9000,
       "waitingTime": "15 min"
-    },
-    {
+  },
+  {
       "productId": "7",
       "storeId": "3",
       "name": "Pollo al curry",
       "categoryId": "5",
       "productImageUrl": "https://i.blogs.es/9ea7a4/pollo_curry-copia/650_1200.jpg",
-      "ingredients": ["Pollo", "Curry", "Leche de coco", "Pimiento", "Cebolla", "Arroz"],
+      "ingredients": "• Pollo • Curry • Leche de coco • Pimiento • Cebolla • Arroz",
       "clarification": "",
       "price": 7000,
       "waitingTime": "30 min"
-    },
-    {
+  },
+  {
       "productId": "8",
       "storeId": "3",
       "name": "Fideos con Albóndigas",
       "categoryId": "4",
       "productImageUrl": "https://imag.bonviveur.com/espaguetis-con-albondigas-spaghetti-and-meatballs.jpg",
-      "ingredients": ["Fideos", "Albóndigas", "Salsa de tomate", "Cebolla", "Ajo", "Aceite de oliva", "Sal", "Pimienta", "Queso rallado (opcional)"],
+      "ingredients": "• Fideos • Albóndigas • Salsa de tomate • Cebolla • Ajo • Aceite de oliva • Sal • Pimienta • Queso rallado (opcional)",
       "clarification": "",
       "price": 8000,
       "waitingTime": "45 min"
-    },
-    {
+  },
+  {
       "productId": "9",
       "storeId": "3",
       "name": "Tacos al pastor",
       "categoryId": "6",
       "productImageUrl": "https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg",
-      "ingredients": ["Carne de cerdo adobada", "Tortillas de maíz", "Piña", "Cebolla", "Cilantro", "Salsa picante"],
+      "ingredients": "• Carne de cerdo adobada • Tortillas de maíz • Piña • Cebolla • Cilantro • Salsa picante",
       "clarification": "",
       "price": 8000,
       "waitingTime": "30 min"
-    }
-  ];
-  
+  }
+];
---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../components/Navbar/Navbar"
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Search Page</h1>
+
+    </div>
+  );
+};
+
+export default Search;
