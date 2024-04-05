80ce59eab4ee3cd0c72453155eeb0a72867ba676
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-05 17:36:31 -0300

    Mensaje de commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Orders from './pages/Orders';
-import Payment from './pages/Payment';
-import Bonus from './pages/Bonus';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Orders from "./pages/Orders";
+import Payment from "./pages/Payment";
+import Bonus from "./pages/Bonus";
+import ChosenProduct from "./pages/ChosenProduct";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/inicio' element={<Home/>}></Route>
-        <Route path='/perfil' element={<Profile/>}></Route>
-        <Route path='/beneficios' element={<Bonus/>}></Route>
-        <Route path='/pedidos' element={<Orders/>}></Route>
-        <Route path='/pagos' element={<Payment/>}></Route>
+        <Route
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          path="/inicio"
+          element={<Home />}
+        ></Route>
+        <Route
+          path="/perfil"
+          element={<Profile />}
+        ></Route>
+        <Route
+          path="/beneficios"
+          element={<Bonus />}
+        ></Route>
+        <Route
+          path="/pedidos"
+          element={<Orders />}
+        ></Route>
+        <Route
+          path="/pagos"
+          element={<Payment />}
+        ></Route>
+        <Route
+          path="/producto-elegido"
+          element={<ChosenProduct />}
+        ></Route>
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,66 +1,82 @@
-import React from 'react'
-import { Link as LinkRouter } from 'react-router-dom';
-import '../styles/footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faUser, faPercent, faClipboardList, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons'
-
-
-
+import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import "../styles/footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faPercent,
+  faClipboardList,
+  faFileInvoiceDollar,
+  faExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 
 const pages = [
   {
-    name: 'Inicio',
-    path: '/inicio',
-    icon: faHouse
-
+    name: "Inicio",
+    path: "/inicio",
+    icon: faHouse,
   },
   {
-    name: 'Perfil',
-    path: '/perfil',
-    icon: faUser
+    name: "Perfil",
+    path: "/perfil",
+    icon: faUser,
   },
   {
-    name: 'Beneficios',
-    path: '/beneficios',
-    icon: faPercent
+    name: "Beneficios",
+    path: "/beneficios",
+    icon: faPercent,
   },
   {
-    name: 'Pedidos',
-    path: '/pedidos',
-    icon: faClipboardList
+    name: "Pedidos",
+    path: "/pedidos",
+    icon: faClipboardList,
   },
   {
-    name: 'Pagos',
-    path: '/pagos',
-    icon: faFileInvoiceDollar
-  }
+    name: "Pagos",
+    path: "/pagos",
+    icon: faFileInvoiceDollar,
+  },
+  {
+    name: "Producto Elegido",
+    path: "/producto-elegido",
+    icon: faExclamation,
+  },
 ];
 
 function Footer() {
   return (
     <>
-      <footer className='footer'>
-        <nav style={{ width: '98%', marginLeft:'1%', marginRight:'1%' }}>
-          <ul style={{ width: '100%', display: 'flex', flexDirection: 'row', columnGap: '20px', justifyContent: 'center' }}>
+      <footer className="footer">
+        <nav style={{ width: "98%", marginLeft: "1%", marginRight: "1%" }}>
+          <ul
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              columnGap: "20px",
+              justifyContent: "center",
+            }}
+          >
             {pages.map((page) => (
-              <li key={page.path} style={{ listStyleType: 'none' }}>
+              <li
+                key={page.path}
+                style={{ listStyleType: "none" }}
+              >
                 <LinkRouter to={page.path}>
-
-                  <FontAwesomeIcon icon={page.icon}
-                    style={{ fontSize: '20px' }} />
-                  <p style={{ fontSize: '14px' }}>{page.name}</p>
+                  <FontAwesomeIcon
+                    icon={page.icon}
+                    style={{ fontSize: "20px" }}
+                  />
+                  <p style={{ fontSize: "14px" }}>{page.name}</p>
                 </LinkRouter>
-              </li>))}
+              </li>
+            ))}
           </ul>
-
         </nav>
-
-
-
-
       </footer>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
