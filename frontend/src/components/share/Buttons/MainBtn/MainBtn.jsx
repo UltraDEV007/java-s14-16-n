57d9300a6c84db39ee0a09862d175d5508186817
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-05 01:00:07 -0600

    refactor: add default type value for button

--- frontend/src/components/share/Buttons/MainBtn/MainBtn.jsx
+++ frontend/src/components/share/Buttons/MainBtn/MainBtn.jsx
@@ -2,13 +2,15 @@ import './MainBtn.css'
 
 export default function MainBtn({
   children,
-  type,
+  type = 'button',
   className,
   disabled,
+  onClick,
 }) {
   return <button {...{
     className: `main-btn${className ? ' ' + className : ''}`,
     type, 
     disabled,
+    onClick,
   }} >{children}</button>
 }
