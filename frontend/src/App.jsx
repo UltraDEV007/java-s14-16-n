6395f42af0300fe9ee5d48b0a057fc9c7113a714
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-05 21:02:02 -0300

    feat: agregando (contexto para manejo global de dataProductos, archivo config para la ApiUrl, construccion dinamica de tarjeta de productos)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AppContext from './context/AppContex';
+import { API_BASE_URL } from './config';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
@@ -13,13 +16,41 @@ import Payout from './components/Payout/Payout';
 import ConfirmPayment from './components/ConfirmPayment/ConfirmPayment';
 import Approved from './components/Approved/Approved';
 
+// conexion al json
+import { products } from './data/products';
+
 function App() {
+  const [dataProducts, setDataProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // codigo para obtener los productos de la api 
+    // axios
+    //   .get(`${API_BASE_URL}/products`)
+    //   .then((response) => {
+    //     console.log('respuesta de la api:response.data')
+    //     console.log(response.data)
+    //     setDataProducts(response.data.content);
+    //     setLoading(false);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error getting data:', error);
+    //     setLoading(false);
+    //   });
+
+    setDataProducts(products);
+    setLoading(false)
+
+  }, []);
+
+
   return (
     <>
-      <Navbar />
+      <AppContext.Provider value={{ dataProducts }}>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Welcome />}></Route>
-          <Route path='/inicio' element={<Home/>}></Route>
+          <Route path='/inicio' element={<Home />}></Route>
           <Route path='/busqueda' >
             <Route path=':mealId' ></Route>
             <Route path='pagar'>
@@ -31,12 +62,17 @@ function App() {
               <Route path='detalles-de-entrega' element={<></>}></Route>
             </Route>
           </Route>
-          <Route path='/perfil' element={<Profile/>}></Route>
-          <Route path='/beneficios' element={<Bonus/>}></Route>
-          <Route path='/pedidos' element={<Orders/>}></Route>
-          <Route path='/pagos' element={<Payment/>}></Route>
+          <Route path='/perfil' element={<Profile />}></Route>
+          <Route path='/beneficios' element={<Bonus />}></Route>
+          <Route path='/pedidos' element={<Orders />}></Route>
+          <Route path='/pagos' element={<Payment />}></Route>
         </Routes>
-      <Footer />
+        <Footer />
+      </AppContext.Provider>
+
+
+
+
     </>
   )
 }
--- frontend/src/components/cardProduct/CardProduct.jsx
+++ frontend/src/components/cardProduct/CardProduct.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from "react-router-dom";
 import './cardProduct.css'
 
 
 function CardProduct({ products }) {
+    const [selectedProduct, setSelectedProduct] = useState();
+    const [goToItem, setGoToItem]= useState(false)
 
+    const handleCardProduct = (itemId) => {
+        if (itemId !== selectedProduct) {
+            setSelectedProduct(itemId);
+            setGoToItem(true)
+        }
+    }
+    // dirigir la navegacion al detalle de producto actualizar path segun endopoint de API
+    // if (goToItem) {
+    //     return <Navigate to={'/productos/' + selectedProduct} />
+    //   }
+    
+   
     return (
         <>
             {products.map((item) => (
-                <div className='cardProduct' key={item.id}>
+                <div className='cardProduct' key={item.id} onClick={() => handleCardProduct(item.id)}>
                     <div className='cardInfo'>
                         <div className='cardInfoText'>
-                            <p>{item.name}</p>
+                            <p style={{ fontWeight: 'bold' }}>{item.name}</p>
                             <p>Nombre Restaurante</p>
                         </div>
                         <div className='cardInfoLogo'>
@@ -18,9 +33,8 @@ function CardProduct({ products }) {
                         </div>
                     </div>
                     <div className='cardImage'>
-                       
-                        <img src='https://s3.abcstatics.com/media/gurmesevilla/2012/01/comida-rapida-casera.jpg' alt='food image'></img>
-                        </div>
+                        <img src={item.image} alt={item.name + ' imagen'} ></img>
+                    </div>
                 </div>
             ))}
 
