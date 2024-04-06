2eb86b421f5b7309a941cd727c6367efa576ec36
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-06 18:30:12 -0500

    style: label font weight

--- frontend/src/components/PaymentMethod/PaymentMethod.jsx
+++ frontend/src/components/PaymentMethod/PaymentMethod.jsx
@@ -2,27 +2,44 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Form from "../share/Form/Form";
 import { Link } from "react-router-dom";
-import './PaymentMethod.css'
+import "./PaymentMethod.css";
 import MainBtn from "../share/Buttons/MainBtn/MainBtn";
 import PaymentCard from "./PaymentCard/PaymentCard";
+import { useState } from "react";
 
 export default function PaymentMethod() {
+  const [active, setActive] = useState(false);
   return (
     <>
-      <Form className={'payment-method'}>
+      <Form className={"payment-method"}>
         <Link to="..">
           <FontAwesomeIcon icon={faArrowLeft} />
         </Link>
         <h4>Elegí tu medio de pago</h4>
 
         <div>
-            <label htmlFor="switch-method">Pago on-line</label>
-            <input type="checkbox" name="switch-method"/>
-            <label htmlFor="switch-method">Efectivo u otros</label>
+          <label
+            htmlFor="switch-method"
+            style={{ fontWeight: !active ? "600" : "100"}}
+          >
+            Pago on-line
+          </label>
+          <input
+            type="checkbox"
+            value={active}
+            name="switch-method"
+            onChange={() => setActive(!active)}
+            />
+          <label
+            htmlFor="switch-method"
+            style={{ fontWeight: active ? "600" : "100"}}
+          >
+            Efectivo u otros
+          </label>
         </div>
 
         <ul>
-            <PaymentCard/>
+          <PaymentCard />
         </ul>
 
         <MainBtn>Ingresar otro medio de pago</MainBtn>
