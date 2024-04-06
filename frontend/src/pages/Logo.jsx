35579dca852249c1ef0b8691e7a9840f22db1ee8
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-06 03:48:34 -0600

    feat: add Welcome

--- frontend/src/pages/Logo.jsx
+++ frontend/src/pages/Logo.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function Welcome() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(navigate, 2e3, 'inicio')
+  })
+
   return (
     <>
       <p>Logo</p>
