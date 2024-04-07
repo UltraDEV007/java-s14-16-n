8fa5e115872fa21af364228dc2b5d2c6c98cef82
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-06 21:18:23 -0300

    The StoreServiceImpl was implemented.

--- backend/src/main/java/com/nocountry/foodlyfinds/model/dto/StoreDTO.java
+++ backend/src/main/java/com/nocountry/foodlyfinds/model/dto/StoreDTO.java
@@ -1,9 +1,13 @@
 package com.nocountry.foodlyfinds.model.dto;
 
+import com.nocountry.foodlyfinds.model.entity.StoreEntity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class StoreDTO {
@@ -21,4 +25,20 @@ public class StoreDTO {
     private String phoneNumber;
 
     private String storeImageUrl;
+
+    public static StoreDTO convertTo(StoreEntity entity) {
+        StoreDTO dto = new StoreDTO();
+        dto.setStoreId(entity.getStoreId());
+        dto.setName(entity.getName());
+        dto.setAddress(entity.getAddress());
+        dto.setPhoneNumber(entity.getPhoneNumber());
+        dto.setStoreImageUrl(entity.getStoreImageUrl());
+        return dto;
+    }
+
+    public static List<StoreDTO> convertList(List<StoreEntity> entityList) {
+        return entityList.stream()
+                .map(StoreDTO::convertTo)
+                .collect(Collectors.toList());
+    }
 }
