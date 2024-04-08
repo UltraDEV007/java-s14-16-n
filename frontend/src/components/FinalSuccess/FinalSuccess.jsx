d21dedbfab3465ed1661e9b836f4f97cd01030c9
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-07 20:08:12 -0500

    feat: final success view

---
+++ frontend/src/components/FinalSuccess/FinalSuccess.jsx
@@ -0,0 +1,10 @@
+import "./FinalSuccess.css";
+
+export default function FinalSuccess() {
+  return (
+    <div className="final-success">
+      <h5>¡Que lo disfrutes!</h5>
+      <img src="https://static.vecteezy.com/system/resources/previews/003/757/448/original/plate-with-traditional-thanksgiving-meal-fork-and-knife-roast-turkey-with-cranberry-sauce-carrots-brussel-sprouts-christmas-or-thanksgiving-festive-food-holiday-meal-illustration-vector.jpg" />
+    </div>
+  );
+}
