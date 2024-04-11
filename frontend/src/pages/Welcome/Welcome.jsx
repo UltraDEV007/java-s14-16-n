8a9c9f34e8fef40d84e71a654b5bb5607c47dae7
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-10 23:51:34 -0600

    Style: add responsive at Welcome

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -13,8 +13,8 @@ export default function Welcome() {
 
   return (
     <main className='welcome'>
-      <svg viewBox="0 0 360 271" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M-131 98.091C105.796 26.3331 542.581 -74.128 395.351 98.091C211.312 313.365 -2.17296 335.634 -131 98.091Z" fill="white" stroke="white" stroke-width="4.21027"/>
+      <svg viewBox="0 0 325 156" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4 56.9332C139.43 16.2339 389.24 -40.745 305.034 56.9332C199.778 179.031 77.6797 191.662 4 56.9332Z" stroke-width="4.21027"/>
       </svg>
       <img src={logo} alt="logo" />
     </main>
