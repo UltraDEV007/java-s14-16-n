ed60bbd81ed11364f3fe3379a9d2b653b9b8e276
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-10 19:50:14 -0600

    refactor: clean ConfirmOrder

--- frontend/src/components/ConfirmOrder/ConfirmOrder.jsx
+++ frontend/src/components/ConfirmOrder/ConfirmOrder.jsx
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 import './confirmorder.css'
 import MainBtn from "../share/Buttons/MainBtn/MainBtn";
 import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
-import Backward from "../share/Buttons/Backward/Backward";
 
 
 function ConfirmOrder() {
@@ -27,12 +26,12 @@ function ConfirmOrder() {
     <>
       <div className="ConfirmContent">
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-          {/* <Link to='/busqueda/pagar/producto-elegido'><FontAwesomeIcon
+          <Link to='/busqueda/pagar/producto-elegido'><FontAwesomeIcon
             className="pointer sizeUp"
             icon={faArrowLeft}
           />
-          </Link> */}
-          <Backward />
+          </Link>
+
           <p>Confirmá tu Pedido</p>
         </div>
 
