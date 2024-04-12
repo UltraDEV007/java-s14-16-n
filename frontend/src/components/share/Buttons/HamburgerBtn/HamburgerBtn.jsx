31d30a3a3f1ad69e6cbcb062bd5ca8ac9664a13a
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-12 12:42:54 -0300

    style: ajustando estilo del navbar y navegacion entre vistas desde la arrow del header (hasta metodo de pago)

--- frontend/src/components/share/Buttons/HamburgerBtn/HamburgerBtn.jsx
+++ frontend/src/components/share/Buttons/HamburgerBtn/HamburgerBtn.jsx
@@ -1,13 +1,16 @@
+import React from 'react'
 import './HamburgerBtn.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 export default function HamburgerBtn() {
   return (
     <button className="hamburger-btn" >
-      <svg viewBox='0 0 100 100'>
+      <FontAwesomeIcon icon={faBars} style={{fontSize:'21px'}}/>
+      {/* <svg viewBox='0 0 100 100'>
         <rect />
         <rect />
         <rect />
-      </svg>
+      </svg> */}
     </button>
   )
 }
