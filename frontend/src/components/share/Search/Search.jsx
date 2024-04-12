75cb96a01a6cbdcef0340b33dd77314dcafbb888
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-12 01:24:59 -0600

    fix: change lang

--- frontend/src/components/share/Search/Search.jsx
+++ frontend/src/components/share/Search/Search.jsx
@@ -1,6 +1,4 @@
 import './Search.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 export default function Search() {
   return (
