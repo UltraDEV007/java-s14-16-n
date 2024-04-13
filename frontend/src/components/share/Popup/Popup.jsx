23745d6df056f33faeda2dc621da97d350bcd041
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-13 02:27:32 -0600

    feat: add Popup

---
+++ frontend/src/components/share/Popup/Popup.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from "react"
+import { useSearchParams } from "react-router-dom"
+import './Popup.css'
+
+export default function Popup({
+  name, 
+  children, 
+  fn, 
+  param = 'popup',
+}) {
+  const dialog = useRef(<></>)
+  const [params, setParams] = useSearchParams()
+  const paramName = params.get(param)
+
+  function close() {
+    setParams('')
+  }
+  
+  useEffect(() => {
+    if (paramName === name) {
+      function listen(e) {
+        if (e.key === 'Escape' || e.target.nodeName === 'DIALOG') {
+          close()
+        }
+      }
+
+      dialog.current.showModal()
+      document.addEventListener('keydown', listen)
+      document.addEventListener('click', listen)
+
+      return () => {
+        document.removeEventListener('click', listen)
+        document.removeEventListener('keydown', listen)
+        dialog.current.close()
+      }
+    }
+  })
+
+  if (fn) fn.current = close
+
+  return <dialog ref={dialog}>
+    {children}
+  </dialog>
+}
