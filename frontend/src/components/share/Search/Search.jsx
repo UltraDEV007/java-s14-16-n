49a54a2b2ba70e18059015e5cc00840a29268394
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-14 00:22:48 -0600

    feat: implement functional Search

--- frontend/src/components/share/Search/Search.jsx
+++ frontend/src/components/share/Search/Search.jsx
@@ -3,6 +3,7 @@ import './Search.css'
 
 export default function Search({
   param = 'q',
+  setPath = '/inicio/',
 }) {
   const [params, setParams] = useSearchParams()
   const navigate = useNavigate()
@@ -10,12 +11,12 @@ export default function Search({
   return (
     <>
       <search className='search-bar'>
-        <button className='magnifier' type="submit" onClick={() => navigate(params.get(param))}/>
+        <button className='magnifier' type="submit" onClick={() => navigate(setPath + params.get(param))}/>
         <input 
           type="search" 
           name="search" 
           placeholder='Buscar' 
-          value={params.get(param)} 
+          value={params.get(param) ?? ''} 
           onChange={(e, value = e.target.value) => setParams(prev => (value ? prev.set(param, value) : prev.delete(param), prev), {replace: true})}
         />
         <button className='filter' type='button' />
