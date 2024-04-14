4d733e12dc7d9836bf5225464d8c1c7fb00f6697
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-13 18:22:28 -0600

    feat: apply params on Search

--- frontend/src/components/share/Search/Search.jsx
+++ frontend/src/components/share/Search/Search.jsx
@@ -1,11 +1,23 @@
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import './Search.css'
 
-export default function Search() {
+export default function Search({
+  param = 'q',
+}) {
+  const [params, setParams] = useSearchParams()
+  const navigate = useNavigate()
+
   return (
     <>
       <search className='search-bar'>
-        <button className='magnifier' type="button" />
-        <input type="search" name="search" placeholder='Buscar'/>
+        <button className='magnifier' type="submit" onClick={() => navigate(params.get(param))}/>
+        <input 
+          type="search" 
+          name="search" 
+          placeholder='Buscar' 
+          value={params.get(param)} 
+          onChange={(e, value = e.target.value) => setParams(prev => (value ? prev.set(param, value) : prev.delete(param), prev), {replace: true})}
+        />
         <button className='filter' type='button' />
       </search >
     </>
