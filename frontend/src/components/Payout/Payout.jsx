17f6f9e2fb5f550f029599e2e6a6d0427e061fb4
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-16 07:40:17 -0600

    feat: add payout high view

--- frontend/src/components/Payout/Payout.jsx
+++ frontend/src/components/Payout/Payout.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Navigate } from "react-router-dom";
 import Form from "../share/Form/Form";
 import MainBtn from "../share/Buttons/MainBtn/MainBtn"
@@ -6,39 +6,36 @@ import { faDollarSign, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Payout.css'
 import { useState } from 'react';
+import ToggleButton from '../share/ToggleButton/ToggleButton';
 
 export default function Payout() {
-  const [sendOrder, setSendOrder]= useState(false)
-  const [addToOrder, setAddToOrder]= useState(false)
-  const handleSendOrder= ()=>{
-    console.log('enviar pedido')
-    setSendOrder(true)
-  }
-  if(sendOrder){
-    return <Navigate to={'/busqueda/pagar/procesando-pago'} />
-  }
-  const handleAdd= ()=>{
-    console.log('agregar al pedido')
-    setAddToOrder(true)
-  }
-  if(addToOrder){
-    return <Navigate to={'/inicio'} />
-  }
+  const navigate = useNavigate()
+  const [money, setMoney]= useState('')
+
   return (
     <>
       <main>
-        <Form className={'cash-stamp'}>
-          <Link to='/busqueda/pagar/medio-de-pago'><FontAwesomeIcon icon={faArrowLeft} /></Link>
-          <h3>Porfavor, especifica el monto con el que pagarás</h3>
-          <hr />
-          <p>valor total de la compra ${'7000'}</p>
+        <Form className={'payout'}>
+          <div className='payment'>
+            <p>Total a pagar</p>
+            <p><FontAwesomeIcon icon={faDollarSign} /> {'17.000'}</p>
+          </div>
+          <h3>Seleccione un método de pago</h3>
+          <ToggleButton options={['Efectivo', 'Pago Online']}/>
+          <p>¿Con qué monto en efectivo desea pagar?</p>
           <label >
-            <FontAwesomeIcon icon={faDollarSign} />
-            <input type="number" name="amount" />
+            <span className={money ? 'transparent' : ''}>Ingresar monto en efectivo</span>
+            <p>Efectivo: $ </p>
+            <input 
+              style={{width: `${money.length}ch`}}
+              type="text" 
+              name="cash"
+              pattern='[0-9\.]+$'
+              value={money} 
+              onChange={(e, value = e.target.value) => setMoney(/^$|[0-9\.]+$/.test(value) ? value : money)}
+            />
           </label>
-          <MainBtn onClick={handleSendOrder}>Hacer pedido</MainBtn>
-          <MainBtn onClick={handleAdd}>Quiero agregar</MainBtn>
-          <hr />
+          <MainBtn onClick={() => navigate('../procesando-pago')} disabled={17000 <= +money.replace('.', '') ? false : true}>Confirmar</MainBtn>
         </Form>
       </main>
     </>
