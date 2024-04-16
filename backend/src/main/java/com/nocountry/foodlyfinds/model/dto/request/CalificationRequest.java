13b829a17177b6d696321d08d6209e22bfcdbd9b
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-15 19:27:32 -0500

    Agg califications , and services, and controller

--- backend/src/main/java/com/nocountry/foodlyfinds/model/dto/request/CalificationRequest.java
+++ backend/src/main/java/com/nocountry/foodlyfinds/model/dto/request/CalificationRequest.java
@@ -2,6 +2,7 @@ package com.nocountry.foodlyfinds.model.dto.request;
 
 import com.nocountry.foodlyfinds.model.entity.StoreEntity;
 import com.nocountry.foodlyfinds.model.entity.UserTblEntity;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,10 +17,6 @@ import lombok.NoArgsConstructor;
 public class CalificationRequest {
 
     private Long calificationId;
-    @NotNull(message = "User is required")
-    private UserTblEntity user;
-    @NotNull(message = "Store is required")
-    private StoreEntity store;
     @NotNull(message = "Value is required")
     private  Integer value;
 
--- backend/src/main/java/com/nocountry/foodlyfinds/model/service/impl/CalificationServiceImpl.java
+++ backend/src/main/java/com/nocountry/foodlyfinds/model/service/impl/CalificationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.nocountry.foodlyfinds.model.service.impl;
 
-import com.nocountry.foodlyfinds.model.dto.response.CalificationResponse;
 import com.nocountry.foodlyfinds.model.entity.CalificationEntity;
 import com.nocountry.foodlyfinds.model.entity.StoreEntity;
 import com.nocountry.foodlyfinds.model.entity.UserTblEntity;
@@ -9,6 +8,7 @@ import com.nocountry.foodlyfinds.model.repository.StoreRepository;
 import com.nocountry.foodlyfinds.model.repository.UserRepository;
 import com.nocountry.foodlyfinds.model.service.CalificationService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,35 +23,21 @@ public class CalificationServiceImpl implements CalificationService {
         this.storeRepository = storeRepository;
     }
 
+    @Transactional
     @Override
-    public CalificationResponse createCalification(Long userId, Long storeId, Integer value){
-        UserTblEntity user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
-        StoreEntity store = storeRepository.findById(storeId).orElseThrow(() -> new EntityNotFoundException("Tienda no encontrada"));
+    public void createCalification(Long userId, Long storeId, Integer value){
+        UserTblEntity user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found with id "+ userId));
+        StoreEntity store = storeRepository.findById(storeId).orElseThrow(() -> new EntityNotFoundException("Store not found with id " + storeId));
 
-        // Crear una nueva calificación y establecer las relaciones con el usuario y la tienda
+        if (value < 0 || value > 1) {
+            throw new IllegalArgumentException("La puntuación debe ser 0 o 1 .");
+        }
+
         CalificationEntity calification = new CalificationEntity();
         calification.setUser(user);
         calification.setStore(store);
         calification.setValue(value);
 
-        // Validar que la puntuación esté dentro del rango permitido (1-10)
-        if (calification.getValue() < 1 || calification.getValue() > 10) {
-            // Manejar el caso de puntuación fuera de rango, lanzar una excepción o retornar un mensaje de error apropiado
-            // Por ejemplo, puedes lanzar una excepción de Validación personalizada
-            throw new IllegalArgumentException("La puntuación debe estar en el rango de 1 a 10.");
-        }
-
-        // Guardar la calificación en la base de datos
-        calification = calificationRepository.save(calification);
-
-        // Convertir la entidad Calificacion a un DTO de respuesta
-        CalificationResponse responseDTO = new CalificationResponse();
-        responseDTO.setCalificationId(calification.getCalificationId());
-        responseDTO.setUser(calification.getUser());
-        responseDTO.setStore(calification.getStore());
-        responseDTO.setValue(calification.getValue());
-
-        return responseDTO;
+        calificationRepository.save(calification);
     }
-
 }
