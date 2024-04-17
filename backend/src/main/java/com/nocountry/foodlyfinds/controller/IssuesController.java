a4f32bdeb5748c0e2ccd3dc7ca97c75535275982
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-17 09:54:16 -0600

    cors origin config

--- backend/src/main/java/com/nocountry/foodlyfinds/controller/IssuesController.java
+++ backend/src/main/java/com/nocountry/foodlyfinds/controller/IssuesController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@CrossOrigin
+@CrossOrigin
 @RequestMapping("api/v1/issues")
 @RequiredArgsConstructor
 public class IssuesController {
--- backend/src/main/java/com/nocountry/foodlyfinds/controller/StoreController.java
+++ backend/src/main/java/com/nocountry/foodlyfinds/controller/StoreController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@CrossOrigin
+@CrossOrigin
 @RequestMapping("api/v1/stores")
 public class StoreController {
 
