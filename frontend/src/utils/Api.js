525b7a163ca774d73f16bdc726a1129946abe841
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-20 09:44:55 -0300

    refactor: ACTUALIZANDO URL DE LA API

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,3 +1,3 @@
 // const BASE_URL = "http://158.180.237.16:8080/api/v1/product/findall"
-const BASE_URL = "http://158.180.237.16:8080/api/v1"
+const BASE_URL = "https://s14-16-n-java-production.up.railway.app/api/v1/"
 
