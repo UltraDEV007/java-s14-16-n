5d6c6d1940e18b53cbdee709e8978e2096ed9de3
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-22 21:17:37 -0500

    feat: navigate to home

--- frontend/src/components/FinalClaim/FinalClaim.jsx
+++ frontend/src/components/FinalClaim/FinalClaim.jsx
@@ -1,11 +1,18 @@
 import "./FinalClaim.css";
-import logo from '../../assets/Logo.svg'
+import logo from "../../assets/Logo.svg";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function FinalClaim() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(navigate, 1e4, "../../../inicio");
+  });
   return (
     <div className="final-claim">
       <p>
-        Sentimos que la tienda te haya hecho pasar un mal momento, pero estamos nosotros asegurándote que:
+        Sentimos que la tienda te haya hecho pasar un mal momento, pero estamos
+        nosotros asegurándote que:
       </p>
       <h5>¡Siempre sales ganando!</h5>
       <div>
