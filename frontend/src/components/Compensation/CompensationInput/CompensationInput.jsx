7eb3b0af53d9585d4c1fb2bc3e30cebdc868311a
UltraDEV007 <LeTuan2024@outlook.com>
2024-04-23 21:13:48 -0500

    feat: compensation validation

--- frontend/src/components/Compensation/CompensationInput/CompensationInput.jsx
+++ frontend/src/components/Compensation/CompensationInput/CompensationInput.jsx
@@ -3,20 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquare, faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-export default function CompensationInput({ item }) {
-  const [isChecked, setCheck] = useState(false);
+export default function CompensationInput({ item , checked}) {
 
   return (
     <li className={item.border && 'card-border'}>
       <label htmlFor="food-type">
         {item.description}
-        {/* <FontAwesomeIcon
-          className={`checkbox-icon${isChecked ? '--active' : ''}`}
-          icon={!isChecked ? faSquare : faSquareCheck}
-          onClick={() => setCheck(!isChecked)}
-        /> */}
       </label>
-      <input type="checkbox" />
+      <input type="checkbox" onChange={checked}/>
     </li>
   );
 }
